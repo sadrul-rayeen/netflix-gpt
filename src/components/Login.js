192e9aec8bb9da3a07135fb2642bcc554ee1cb02
Adil <adil@Adils-MacBook-Air.local>
2024-08-21 23:28:20 +0530

    Router

---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/031c42b9-0c81-4db5-b980-0160765188e9/27f1b15d-79ed-43ca-8982-7faa9e4aa388/IN-en-20240819-TRIFECTA-perspective_WEB_3c576fa6-cd23-46b6-ac3f-1ad2bb0f66fb_small.jpg"
+          srcset="https://assets.nflxext.com/ffe/siteui/vlv3/031c42b9-0c81-4db5-b980-0160765188e9/27f1b15d-79ed-43ca-8982-7faa9e4aa388/IN-en-20240819-TRIFECTA-perspective_WEB_3c576fa6-cd23-46b6-ac3f-1ad2bb0f66fb_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/031c42b9-0c81-4db5-b980-0160765188e9/27f1b15d-79ed-43ca-8982-7faa9e4aa388/IN-en-20240819-TRIFECTA-perspective_WEB_3c576fa6-cd23-46b6-ac3f-1ad2bb0f66fb_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/031c42b9-0c81-4db5-b980-0160765188e9/27f1b15d-79ed-43ca-8982-7faa9e4aa388/IN-en-20240819-TRIFECTA-perspective_WEB_3c576fa6-cd23-46b6-ac3f-1ad2bb0f66fb_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="absolute p-12 w-3/12 bg-black my-36 mx-auto right-0 left-0 text-white">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-2 my-2 w-full"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-2 my-2 w-full"
+        />
+        <button className="p-2 my-4 bg-red-700 w-full">Sing In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
